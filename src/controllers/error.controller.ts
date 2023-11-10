97c631f69d6f0e8bf8400186f05fde4297e70601
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-11-10 11:09:19 +0500

    modify Error Handler, for development & production

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -3,7 +3,32 @@ import { Request, Response, NextFunction } from "express";
 interface IError extends Error {
   statusCode?: number;
   status?: string;
+  isOperational?: boolean;
 }
+
+const sendErrorDev = (err: IError, res: Response) => {
+  res.status(err.statusCode as number).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err: IError, res: Response) => {
+  if (err.isOperational) {
+    res.status(err.statusCode as number).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log("Error 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
+};
 
 export default (
   err: IError,
@@ -13,6 +38,12 @@ export default (
 ) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 
   res.status(err.statusCode as number).json({
     status: err.status,
