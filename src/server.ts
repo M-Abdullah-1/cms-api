233b83cbfa8e788c83a2c713180ade28dfa20a43
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-11-11 23:01:58 +0500

    create config file
    Before we configure the env file directely into the server file, but now I create seprate file for env configuration and import it into server file.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.join(__dirname, "../config.env") });
+import { DATABASE_PASSWORD, PORT } from "./config";
 
 import app from "./app";
 import { connectToDatabase } from "./utils/database.util";
 
 // Call the function with the provided database password
-connectToDatabase(process.env.DATABASE_PASSWORD as string);
+connectToDatabase(DATABASE_PASSWORD as string);
 
 /**
  *  Starts the server on the specfied port.
@@ -15,6 +12,6 @@ connectToDatabase(process.env.DATABASE_PASSWORD as string);
  *  @param {number} - The port number on the server should listen.
  *  @param {Function} - The callback function to be executed when the server is running.
  */
-app.listen(process.env.PORT, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
