75eec509c7e00aefc32fb24dcd61b898d9dd928f
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-12-11 15:37:35 +0500

    save article into DB

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import catchAsync from "../utils/catchAsync.util";
+import articleModel from "../models/article.model";
 
 export const createArticle = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
+    const article = await articleModel.create(req.body);
+    return res.status(200).json({
+      status: "ok",
+      data: {
+        article,
+      },
+    });
   }
 );
