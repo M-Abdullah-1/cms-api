31246514b2175af8d5f2ba6ddfe4229ed10fff1b
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-12-13 14:01:55 +0500

    configure nested routes between article and author

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import * as articleController from "./../controllers/article.controller";
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
+router.route("/").post(articleController.createArticle);
 router.route("/:articleId").put(articleController.updateArticle);
 
 export default router;
--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import articleRoute from "./../routes/article.route";
 import * as blogController from "./../controllers/blog.controller";
 
 const router = Router();
+
+router.use("/author/:authorId/article", articleRoute);
 
 router.route("/author/:authorId").post(blogController.createArticle);
 
