50619d25d7d3f97c13fd40823af77c0ee05e542c
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-12-13 13:36:03 +0500

    create API to updateArticle

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -23,3 +23,26 @@ export const createArticle = catchAsync(
     });
   }
 );
+
+export const updateArticle = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { articleId } = req.params;
+    const { heading, description, category } = req.body;
+    const updatedArticle = await articleModel.findByIdAndUpdate(
+      { _id: articleId },
+      {
+        heading,
+        description,
+        category,
+      },
+      { new: true, runValidators: true }
+    );
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        updatedArticle,
+      },
+    });
+  }
+);
--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -4,5 +4,6 @@ import * as blogController from "./../controllers/blog.controller";
 const router = Router();
 
 router.route("/author/:authorId").post(blogController.createArticle);
+router.route("/article/:articleId").put(blogController.updateArticle);
 
 export default router;
